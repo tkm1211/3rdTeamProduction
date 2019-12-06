fdfdb9ac9950c2e482cee8414b5b527db6fc6264
tkm1211 <tkm.1239@gmail.com>
2019-12-06 17:50:12 +0900

    球面線形補間の関数を追加

--- Sources/GameSources/BG.h
+++ Sources/GameSources/BG.h
@@ -21,3 +21,36 @@ public:
 	void UnInit();
 
 };
+
+
+DirectX::XMFLOAT3 SphereLinear // 戻り値 : 補間座標
+(
+	DirectX::XMFLOAT3 _start, // ベクトル
+	DirectX::XMFLOAT3 _end,   // ベクトル
+	float t					  // 補間値（0.0f ～ 1.0f）
+)
+{
+	DirectX::XMVECTOR start, end;
+	start = DirectX::XMVector3Normalize(DirectX::XMVectorSet(_start.x, _start.y, _start.z, 1.0f));
+	end = DirectX::XMVector3Normalize(DirectX::XMVectorSet(_end.x, _end.y, _end.z, 1.0f));
+
+	float angle = acosf(DirectX::XMVectorGetX(DirectX::XMVector3Dot(start, end)));
+
+	float sinSita = sinf(angle);
+
+	float startPoint = sinf(angle * (1 - t));
+	float endPoint = sinf(angle * t);
+
+	DirectX::XMFLOAT3 startFloat3, endFloat3;
+	DirectX::XMStoreFloat3(&startFloat3, start);
+	DirectX::XMStoreFloat3(&endFloat3, end);
+
+	float len = sqrtf(_start.x * _start.x + _start.y * _start.y + _start.z * _start.z);
+
+	DirectX::XMFLOAT3 out;
+	out.x = ((startPoint * startFloat3.x + endPoint * endFloat3.x) / sinSita) * len;
+	out.y = ((startPoint * startFloat3.y + endPoint * endFloat3.y) / sinSita) * len;
+	out.z = ((startPoint * startFloat3.z + endPoint * endFloat3.z) / sinSita) * len;
+
+	return out;
+}
