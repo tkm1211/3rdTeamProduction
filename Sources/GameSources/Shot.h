06353be8b5b25fb315b3cb5458c15a10051ce760
noritsugi <aquatimez74247@gmail.com>
2019-12-06 12:11:15 +0900

    12/06 12:11

---
+++ Sources/GameSources/Shot.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "OBJ3D.h"
+#define SPPED_ARROW 30
+
+class Shot
+{
+public:
+	Shot(DirectX::XMFLOAT3 pPos, DirectX::XMFLOAT3 ePos);
+	~Shot() {};
+
+	void Update();
+	void Draw();
+
+	OBJ3D GetModelData() { return modelData; };
+private:
+
+	OBJ3D modelData;
+	DirectX::XMFLOAT3 pVec;
+
+};
