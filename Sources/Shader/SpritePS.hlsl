e9f756e8f294fb39e13063a67653240d8d80ddb1
tkm1211 <tkm.1239@gmail.com>
2019-12-13 04:05:33 +0900

    spritePS修正

--- Sources/Shader/SpritePS.hlsl
+++ Sources/Shader/SpritePS.hlsl
@@ -8,7 +8,7 @@ float4 main( VS_OUT pin ) : SV_TARGET
 {
 	float4 color = diffuseMap.Sample( diffuseMapSamplerState, pin.texcoord ) * pin.color;
 
-	float4 resultColor = ScreenFilter(color, bright, contrast, saturate, screenColor);
+	//float4 resultColor = ScreenFilter(color, bright, contrast, saturate, screenColor);
 
-	return resultColor;
+	return color;
 }
